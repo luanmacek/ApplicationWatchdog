32428ccc37688f16ee9e7d029cbb0d50d96fe965
Luan Macek <luanryuu2001@gmail.com>
2021-05-25 13:57:10 +0200

    Add files via upload

---
+++ ApplicationWatchdog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationWatchdog
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //#if DEBUG
+            //ApplicationWatchdogService aws = new ApplicationWatchdogService();
+            //aws.OnDebug();
+            //#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ApplicationWatchdogService()
+            };
+            ServiceBase.Run(ServicesToRun);
+            //#endif
+        }
+    }
+}
